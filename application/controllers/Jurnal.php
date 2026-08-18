cde9562df239318539905d0fb6cb96d4388850de
alief572 <146901652+alief572@users.noreply.github.com>
2026-08-18 15:35:03 +0700

    Fix: BUK, BUM dan JV

--- application/controllers/Jurnal.php
+++ application/controllers/Jurnal.php
@@ -61,6 +61,10 @@ class Jurnal extends CI_Controller
 		// 		$thn_aktif			= substr($tgl_periode_aktif, 3, 4);
 		// 	}
 		// }
+
+		// print_r($thn_aktif . ' - ' . $bln_aktif);
+		// exit;
+
 		$data['data_listjv']		= $this->Jurnal_model->list_jv($bln_aktif, $thn_aktif);
 
 		$this->load->view('jurnal/v_list_jv', $data);
